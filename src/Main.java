d29975e66e94cd36ecb0bd95b67bd0d6b18d6d05
olhaLK <olga.lashuck@gmail.com>
2024-11-18 21:26:32 +0100

    Changes feature-branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-                System.out.println("Hello, GitHub Desktop!");
+        System.out.println("Hello, GitHub Desktop!");
+
+        int a = 5;
+        int b = 10;
+        int sum = addNumbers(a, b);
+
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+    public static int addNumbers(int a, int b) {
+        return a + b;
     }
 }
